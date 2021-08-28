447ce7257b0e7d0e378405e5c4180c1117615f4e
shaochenheng <1010716589@qq.com>
2021-08-29 00:50:10 +0800

    update js save-write

--- test.js
+++ test.js
@@ -1,60 +1,27 @@
-function myfun(a, b) {
-	return a * b;
-	}
-var a = myfun(1, 3);
-console.log(a)
-
-function funSave() {
-var id = 'number1';
-var name = 'hello'
-var str = '{mydata:[' + '{id:' + id + ',name:' + name + '}' + ']}';
-str = "{MyData:[{id:'" + id + "',name:'" + name + "'}]}";
-//var json = eval('(' + str + ')');
-var fso, tf;
-try{
-fso = new ActiveXObject("Scripting.FileSystemObject");
-      tf = fso.CreateTextFile("test.json", true);
-      tf.WriteLine(str);
-}catch(err){
-      }finally{
-      tf.Close();
-      }
-}
-
-
-function funSearch() {
-var fso, ts, s;
-var ForReading = 1;
-try{
-fso = new ActiveXObject("Scripting.FileSystemObject");
-      ts = fso.OpenTextFile("test.json", ForReading);
-      s = ts.ReadLine();
-      var json = eval('(' + s + ')');
-      alert(json.MyData[0].id);
-}catch(err){
-}finally{
-ts.Close();
-}
-}
-import fs from 'fs';
-const fs = require('fs');
-
-// create a JSON object
-const user = {
-    "id": 1,
-    "name": "John Doe",
-    "age": 22
-};
-
-// convert JSON object to string
-const data = JSON.stringify(user);
-
-// write JSON string to a file
-fs.writeFile('user.json', data, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("JSON data is saved.");
+new Vue ({
+    el: '#vue-app',
+    data: {
+        name: '',
+        last: '',
+        index: 0,
+        grade: 0,
+        arr: []
+    },
+  
+    methods: {
+        add: function (e) {
+            this.arr.push({first: this.name, lastn: this.last, index: this.index, grade: this.grade});
+            console.log(1);
+        },
+        saveFile1: function() {
+          const data = JSON.stringify(this.arr)
+          window.localStorage.setItem('arr', data);
+          console.log(JSON.parse(window.localStorage.getItem('arr')));
+        },
+        saveFile: function() {
+            const data = JSON.stringify(this.arr)
+            window.localStorage.setItem('arr', data);
+            console.log(JSON.parse(window.localStorage.getItem('arr')))
+        }
+  }
 });
-
-
